147f331a16d56c1375e80e0611d7ad50fa131bb5
Nathan Loveless <nathansl2003@yahoo.com>
2020-03-31 14:34:18 -0400

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/Nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <h1>React, Redux and Firestore</h1>
-      </div>
+      <>
+        <NavBar />
+        <Container className="main">     
+          <EventDashboard />
+        </Container>
+      </>
     );
   }
 }
