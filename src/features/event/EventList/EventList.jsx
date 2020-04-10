620d7a5fb5f71b33c05c29ad0d458156a3adf07b
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-10 09:39:40 -0400

    Section 5- CRUD operations in React

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,10 +3,15 @@ import EventListItem from './EventListItem'
 
 class EventList extends Component {
     render() {
+        const {events, selectedEvent, deleteEvent} = this.props;
         return (
             <>
-            {this.props.events.map(event => (
-                <EventListItem key={event.id} event={event} />
+            {events.map(event => (
+                <EventListItem 
+                    key={event.id} 
+                    event={event} 
+                    selectEvent={selectedEvent} 
+                    deleteEvent={deleteEvent} />
             )
             )}
             </>
