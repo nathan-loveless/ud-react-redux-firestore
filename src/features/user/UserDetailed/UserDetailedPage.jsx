26b90c36be7142c12479c7d3798d30755795d3b1
Nathan Loveless <nathansl2003@yahoo.com>
2020-06-23 18:35:32 -0400

    Section 6 Complete

---
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const UserDetailedPage = () => {
+    return (
+        <div>
+            <h1>User detailed dage</h1>
+        </div>
+    )
+}
+
+export default UserDetailedPage;
