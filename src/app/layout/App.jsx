668f956770b0b6429250956fc7b78f166fbba8ce
Nathan Loveless <nathansl2003@yahoo.com>
2020-06-30 16:39:03 -0400

    Completed Section 7

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,33 +1,38 @@
-import React, {Component, Fragment} from 'react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/Nav/NavBar/NavBar';
-import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
-import  HomePage  from '../../features/home/HomePage';
-import EventDetailedPage  from '../../features/event/EventDetailed/EventDetailedPage';
-import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
-import  PeopleDashboard  from '../../features/user/PeopleDashboard/PeopleDashboard';
-import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
-import EventForm from '../../features/event/EventForm/EventForm';
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/Nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Route exact path='/' component={HomePage} /> 
-        <Route path='/(.+)' render={() => (
-      <Fragment>
-      <NavBar />
-      <Container className="main">        
-        <Route path='/events' component={EventDashboard} /> 
-        <Route path='/events/:id' component={EventDetailedPage} /> 
-        <Route path='/people' component={PeopleDashboard} /> 
-        <Route path='/profile/:id' component={UserDetailedPage} /> 
-        <Route path='/settings' component={SettingsDashboard} />    
-        <Route path='/createEvent' component={EventForm} /> 
-      </Container>
-      </Fragment>
-        )}/>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+                <Route path="/test" component={TestComponent} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+
+console.log(store.getState());
+
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    , rootEl);
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
 
-if(module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    })
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
